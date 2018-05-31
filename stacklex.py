386a9b9985704ebeb5ee36eef041aeb22cf9f35d
Juncker <Sheldon.Juncker@ingrammicro.com>
2018-05-31 10:46:38 -0400

    Separated lexer and parser into separate files.

---
+++ stacklex.py
@@ -0,0 +1,42 @@
+import lib.ply.lex as lex
+
+tokens = (
+	'NAME', 'NUMBER',
+	'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS',
+	'LPAREN', 'RPAREN',
+)
+
+# Tokens
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_EQUALS = r'='
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+def t_NUMBER(t):
+	r'\d+'
+	t.value = int(t.value)
+	return t
+
+
+# Ignored characters
+t_ignore = " \t"
+
+
+def t_newline(t):
+	r'\n+'
+	t.lexer.lineno += t.value.count("\n")
+
+
+def t_error(t):
+	print("Illegal character '%s'" % t.value[0])
+	t.lexer.skip(1)
+
+
+def testLexing():
+	lex.lex()
